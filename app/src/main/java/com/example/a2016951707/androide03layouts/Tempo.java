4b9a89be1016d60ba276263bbdf1c60861428d9d
gabriel veazey meira <a2016951707@la0012217514.infocoltec.local>
2018-03-15 17:27:44 -0300

    Adicionando arquivos

---
+++ app/src/main/java/com/example/a2016951707/androide03layouts/Tempo.java
@@ -0,0 +1,13 @@
+package com.example.a2016951707.androide03layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tempo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tempo);
+    }
+}
